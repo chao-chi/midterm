4f597eb3c9bb8e09275eaff299ed7f89dd9ac352
olivialin21 <s110819023@stu.ntue.edu.tw>
2020-11-01 23:25:09 +0800

    tour-add-function

---
+++ js/tour.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.tour_video').on('ended',function(){
+        $('.tags').fadeIn(500);
+    });
+    
+    // $('.tag_waterfall').click(function(){
+    //     $('title').html();
+    // });
+});
