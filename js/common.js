c1246fcefc636b1e9460365d21e9a7182d613a0f
olivialin21 <s110819023@stu.ntue.edu.tw>
2020-12-22 15:42:54 +0800

    header ripple animate done

--- js/common.js
+++ js/common.js
@@ -1,10 +1,7 @@
-const ANIMATEDCLASSNAME = "animated";
 const ELEMENTS = document.querySelectorAll(".HOVER");
 const ELEMENTS_SPAN = [];
 
-ELEMENTS.forEach((element, index) => {
-  let addAnimation = false;
-  
+ELEMENTS.forEach((element, index) => {  
 	// If The span element for this element does not exist in the array, add it.
 	if (!ELEMENTS_SPAN[index])
 		ELEMENTS_SPAN[index] = element.querySelector("span");
@@ -12,9 +9,6 @@ ELEMENTS.forEach((element, index) => {
 	element.addEventListener("mouseover", e => {
 		ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + "px";
 		ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
-
-		// Add an animation-class to animate via CSS.
-		if (addAnimation) element.classList.add(ANIMATEDCLASSNAME);
 	});
 
 	element.addEventListener("mouseout", e => {
