fde0c577b79c3544357147a640e57144d7f24a9d
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-01-06 20:16:26 +0800

    contact us less firebase

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    let $title=$("#title");
+    firebase.initializeApp({
+        apiKey: "AIzaSyDch_dCyYYSvGToPaxIgkK6XEYL3NiRnL4",
+        authDomain: "f2e-endterm.firebaseapp.com",
+        projectId: "f2e-endterm",
+        storageBucket: "f2e-endterm.appspot.com",
+        messagingSenderId: "49301175365",
+        appId: "1:49301175365:web:86807d6ec829e5f8e2b72c",
+        measurementId: "G-MG7FQY3CFF"
+    });
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    
+    const $name = $("#name");
+    const $email = $("email");
+    const $message = $("message");
+    let docRef = firebase.firestore();
+    let messagesRef = docRef.collection("contactUs");
+    
+    $("#btn_submit").click(function(){
+        let name_v = $name.val();
+        let email_v =$email.val();
+        let message_v = $message.val();
+        
+        messagesRef.add({
+            "name":name_v,
+            "email":email_v,
+            "message":message_v
+        });
+        $name.val('');
+        $email.val('');
+        $message.val('');
+    });
+});
